855fa9cbb8a9e0f772e812f676e8a42d2187eed8
mirshad95 <85664496+mirshad95@users.noreply.github.com>
2021-06-10 18:15:14 +0530

    Add files via upload

---
+++ idandship.py
@@ -0,0 +1,10 @@
+for i  in range(int(input())):
+    k =input()
+    if k=='b' or k=='B':
+        print("BattleShip")
+    if k=='c' or k=='C':
+        print('Cruiser')
+    if k=='d' or k=='D':
+        print('Destroyer')
+    if  k=='f' or k=="F":
+        print("Frigate")
